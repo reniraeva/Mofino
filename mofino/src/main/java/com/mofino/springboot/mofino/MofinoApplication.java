2d4a5f8245b5fd727b5f7551cfc01a6a8d70f4f9
reniraeva <reneta.ts.ilieva@gmail.com>
2022-03-21 15:46:03 +0200

    Start project

---
+++ mofino/src/main/java/com/mofino/springboot/mofino/MofinoApplication.java
@@ -0,0 +1,13 @@
+package com.mofino.springboot.mofino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MofinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MofinoApplication.class, args);
+	}
+
+}
