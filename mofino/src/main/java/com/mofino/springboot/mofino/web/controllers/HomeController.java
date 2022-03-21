d5c1262da1f766e75cfc27b1eee75735e8f539df
reniraeva <reneta.ts.ilieva@gmail.com>
2022-03-21 16:14:22 +0200

    Add HTML

---
+++ mofino/src/main/java/com/mofino/springboot/mofino/web/controllers/HomeController.java
@@ -0,0 +1,33 @@
+package com.mofino.springboot.mofino.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public ModelAndView index(ModelAndView modelAndView, HttpSession session){
+
+        if (session.getAttribute("username") != null){
+            modelAndView.setViewName("redirect:/home");
+        }else {
+            modelAndView.setViewName("index.html");
+        }
+        return modelAndView;
+    }
+
+    @GetMapping ("/home")
+    public ModelAndView home(ModelAndView modelAndView, HttpSession session){
+
+        if (session.getAttribute("username") == null){
+            modelAndView.setViewName("redirect:/login");
+        }else {
+            modelAndView.setViewName("static/home.html");
+        }
+        return modelAndView;
+    }
+}
